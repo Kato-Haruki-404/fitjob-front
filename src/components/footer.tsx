52a0773ee6e9a048d73a2f78a3db398e0cbfe424
aomona <yuka41623@gmail.com>
2026-02-02 01:00:17 +0900

    feat(ui): Make footer responsive on small screens

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,24 +3,24 @@ import Link from "next/link";
 export default function Footer() {
 	return (
 		<footer className="p-10 flex flex-col bg-foreground text-background font-medium gap-16">
-			<div className="flex justify-between">
+			<div className="flex flex-col gap-5 justify-between md:flex-row md:gap-0">
 				<Link className="text-2xl" href="/">
 					FitJob
 				</Link>
 				<nav>
-					<ul className="flex gap-5 items-center">
+					<ul className="flex gap-3 md:gap-5 items-center text-xs md:text-base">
 						<li>
 							<Link href="/">ホーム</Link>
 						</li>
-						<li aria-hidden="true" className="h-5 w-0.5 bg-white" />
+						<li aria-hidden="true" className="h-4 md:h-5 w-0.5 bg-white" />
 						<li>
 							<Link href="favorite">お気に入り</Link>
 						</li>
-						<li aria-hidden="true" className="h-5 w-0.5 bg-white" />
+						<li aria-hidden="true" className="h-4 md:h-5  w-0.5 bg-white" />
 						<li>
 							<Link href="mypage">マイページ</Link>
 						</li>
-						<li aria-hidden="true" className="h-5 w-0.5 bg-white" />
+						<li aria-hidden="true" className="h-4 md:h-5  w-0.5 bg-white" />
 						<li>
 							<Link href="/">求人募集</Link>
 						</li>
