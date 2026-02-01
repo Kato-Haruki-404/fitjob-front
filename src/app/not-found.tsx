6d5b98a58bdb8bb68664026c61e98eacef0dbe23
aomona <yuka41623@gmail.com>
2026-02-01 23:35:32 +0900

    style(ui): Adjust responsive typography in 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,8 +4,10 @@ import Button from "@/components/ui/button";
 export default function NotFound() {
 	return (
 		<div className="flex flex-col items-center justify-center gap-8 h-full">
-			<h1 className="text-6xl font-bold">404 Not Found</h1>
-			<p className="text-2xl font-bold">ページが見つかりませんでした</p>
+			<h1 className="text-3xl md:text-6xl font-bold">404 Not Found</h1>
+			<p className="text-l md:text-2xl font-bold">
+				ページが見つかりませんでした
+			</p>
 
 			<Button asChild variant="link">
 				<Link href="/">トップに戻る</Link>
