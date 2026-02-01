95508f0632242898214e9aed16f7fda264022bad
aomona <yuka41623@gmail.com>
2026-02-02 03:57:26 +0900

    feat(ui): Add select and textarea components

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -2,6 +2,7 @@ import {
 	Field as HeadlessField,
 	Input as HeadlessInput,
 	Label as HeadlessLabel,
+	Textarea as HeadlessTextarea,
 } from "@headlessui/react";
 import type { ComponentPropsWithoutRef } from "react";
 
@@ -9,9 +10,15 @@ type FieldProps = ComponentPropsWithoutRef<typeof HeadlessField>;
 type LabelProps = ComponentPropsWithoutRef<typeof HeadlessLabel>;
 type InputProps = ComponentPropsWithoutRef<typeof HeadlessInput> &
 	React.InputHTMLAttributes<HTMLInputElement>;
+type SelectProps = ComponentPropsWithoutRef<typeof HeadlessInput> &
+	React.SelectHTMLAttributes<HTMLSelectElement>;
+type TextareaProps = ComponentPropsWithoutRef<typeof HeadlessTextarea> &
+	React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 
 export function Field({ ...props }: FieldProps) {
-	return <HeadlessField className="flex flex-col gap-3" {...props} />;
+	return (
+		<HeadlessField className="flex flex-col gap-3 flex-1 min-w-0" {...props} />
+	);
 }
 
 export function Label({ ...props }: LabelProps) {
@@ -21,6 +28,27 @@ export function Label({ ...props }: LabelProps) {
 export function Input({ ...props }: InputProps) {
 	return (
 		<HeadlessInput
+			className="py-3 px-5 text-3 border border-[#D7D7D7] -outline-offset-1 rounded-xl font-normal"
+			{...props}
+		/>
+	);
+}
+
+export function Select({ ...props }: SelectProps) {
+	return (
+		<HeadlessInput
+			as="select"
+			className="py-3 px-5 text-3 border border-[#D7D7D7] -outline-offset-1 rounded-xl"
+			{...props}
+		>
+			{props.children}
+		</HeadlessInput>
+	);
+}
+
+export function Textarea({ ...props }: TextareaProps) {
+	return (
+		<HeadlessTextarea
 			className="py-3 px-5 text-3 border border-[#D7D7D7] -outline-offset-1 rounded-xl"
 			{...props}
 		/>
