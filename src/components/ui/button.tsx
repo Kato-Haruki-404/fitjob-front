688b7ac9002e45b6ce46c90a85ac5bf322556df7
aomona <yuka41623@gmail.com>
2026-02-01 21:17:18 +0900

    feat(ui): Allow native props and className on button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,17 +1,26 @@
 import { Button as HeadlessButton } from "@headlessui/react";
 import { clsx } from "clsx";
+import type { ComponentPropsWithoutRef } from "react";
 import { twMerge } from "tailwind-merge";
 
 type Varient = "primary" | "outline" | "link" | "ghost";
 
-type ButtonProps = {
+type ButtonProps = Omit<
+	ComponentPropsWithoutRef<typeof HeadlessButton>,
+	"className"
+> & {
 	variant?: Varient;
-	children: React.ReactNode;
+	className?: string;
 };
 
-export default function Button({ variant = "primary", children }: ButtonProps) {
+export default function Button({
+	variant = "primary",
+	children,
+	className,
+	...props
+}: ButtonProps) {
 	const baseClasses =
-		"py-3 px-10 font-bold text-base rounded-xl hover:opacity-70 cursor-pointer transition-all duration-200 ease-in-out";
+		"py-3 px-10 font-bold text-base rounded-xl hover:opacity-70 cursor-pointer transition-all duration-200 ease-in-out disabled:opacity-60 disabled:cursor-not-allowed";
 	const variantClasses = {
 		primary: "bg-main text-white",
 		outline: "outline -outline-offset-1",
@@ -20,7 +29,10 @@ export default function Button({ variant = "primary", children }: ButtonProps) {
 	}[variant];
 
 	return (
-		<HeadlessButton className={twMerge(clsx(baseClasses, variantClasses))}>
+		<HeadlessButton
+			className={twMerge(clsx(baseClasses, variantClasses, className))}
+			{...props}
+		>
 			{children}
 		</HeadlessButton>
 	);
