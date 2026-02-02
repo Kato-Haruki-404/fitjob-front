bf5848fc49e4f0883b876853032756ea739d2eb6
aomona <yuka41623@gmail.com>
2026-02-02 15:25:47 +0900

    feat(ui): Add category grid component

---
+++ src/components/top/categorieGrid.tsx
@@ -0,0 +1,38 @@
+import { ChevronRight } from "lucide-react";
+import Link from "next/link";
+
+type CategoryItem = {
+	label: string;
+	href: string;
+};
+
+type CategorieGridProps = {
+	items: CategoryItem[];
+};
+
+export default function CategorieGrid({ items }: CategorieGridProps) {
+	return (
+		<div className="grid grid-cols-1 md:grid-cols-2 overflow-hidden rounded-xl bg-[#D7D7D7] gap-0.5 p-0 border-2 border-[#D7D7D7]">
+			{items.map((item, index) => (
+				<CategoryLink key={`${item.label}-${index}`} href={item.href}>
+					{item.label}
+				</CategoryLink>
+			))}
+		</div>
+	);
+}
+
+function CategoryLink({
+	children,
+	href,
+}: {
+	children: React.ReactNode;
+	href: string;
+}) {
+	return (
+		<Link className="flex justify-between py-3 px-5 bg-background" href={href}>
+			{children}
+			<ChevronRight />
+		</Link>
+	);
+}
