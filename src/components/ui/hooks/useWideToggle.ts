13b3d3a5958c4ddaab1863cb8c882a0b866c55cc
aomona <yuka41623@gmail.com>
2026-02-03 18:34:50 +0900

    refactor(ui): Remove unused wide toggle hook

--- src/components/ui/hooks/useWideToggle.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-export function useWideToggle() {
-	const [isTrue, setIsTrue] = useState(false);
-	const toggleOn = () => setIsTrue(true);
-	const toggleOff = () => setIsTrue(false);
-	return { isTrue, toggleOn, toggleOff };
-}
