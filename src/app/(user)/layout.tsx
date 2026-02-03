98f15011a4a3727d658d4e3cc882f0334fbba68f
aomona <yuka41623@gmail.com>
2026-02-04 01:57:44 +0900

    feat(layout): Add go back header to user layout

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,10 +1,12 @@
 import Footer from "@/components/footer";
+import { GoBackHeader } from "@/components/header";
 
 export default function UserLayout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<>
+			<GoBackHeader />
 			<main className="flex-1">{children}</main>
 			<Footer />
 		</>
