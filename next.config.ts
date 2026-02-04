f71e12ce3d94e4e294f2a1caafea6d62c813598d
aomona <yuka41623@gmail.com>
2026-02-05 02:46:36 +0900

    chore(next): allow job image host

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	reactCompiler: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "api.fitjob-back.orb.local",
+			},
+		],
+	},
 };
 
 export default nextConfig;
